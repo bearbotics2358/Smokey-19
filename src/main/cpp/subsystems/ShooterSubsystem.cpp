a1891926a0e5a4b9459f02defbe0b794f4ea10f1
Brock Atchison <aldryd@gmail.com>
2026-02-03 17:47:58 -0600

    Add a flywheel simulator to the ShooterSubsystem

--- src/main/cpp/subsystems/ShooterSubsystem.cpp
+++ src/main/cpp/subsystems/ShooterSubsystem.cpp
@@ -1,7 +1,31 @@
 #include "subsystems/ShooterSubsystem.h"
 
+#include "bearlog/bearlog.h"
+
+#include <frc/RobotController.h>
+#include <frc/simulation/BatterySim.h>
+#include <frc/simulation/RoboRioSim.h>
+
 ShooterSubsystem::ShooterSubsystem() {
 }
 
 void ShooterSubsystem::Periodic() {
+    BearLog::Log("Flywheel/Speed", units::revolutions_per_minute_t(m_FlywheelMotor.GetVelocity().GetValue()));
+}
+
+void ShooterSubsystem::SimulationPeriodic() {
+    auto& flywheel_sim = m_FlywheelMotor.GetSimState();
+    flywheel_sim.SetSupplyVoltage(frc::RobotController::GetBatteryVoltage());
+
+    auto motor_voltage = flywheel_sim.GetMotorVoltage();
+    m_FlywheelSimModel.SetInputVoltage(motor_voltage);
+
+    // Simulate the 20ms run in the simulation model
+    m_FlywheelSimModel.Update(20_ms);
+
+    frc::sim::RoboRioSim::SetVInVoltage(frc::sim::BatterySim::Calculate({m_FlywheelSimModel.GetCurrentDraw()}));
+
+    // Update the simulated state for the flywheel motor
+    flywheel_sim.SetRotorVelocity(m_FlywheelSimModel.GetAngularVelocity());
+    flywheel_sim.SetRotorAcceleration(m_FlywheelSimModel.GetAngularAcceleration());
 }
--- src/main/include/subsystems/ShooterSubsystem.h
+++ src/main/include/subsystems/ShooterSubsystem.h
@@ -1,9 +1,34 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <frc/simulation/FlywheelSim.h>
+#include <frc/system/plant/LinearSystemId.h>
+
+#include <ctre/phoenix6/TalonFX.hpp>
 
 class ShooterSubsystem : public frc2::SubsystemBase {
 public:
     ShooterSubsystem();
     void Periodic() override;
+    void SimulationPeriodic() override;
+
+private:
+    static constexpr int kFlywheelMotorId = 2;
+    ctre::phoenix6::hardware::TalonFX m_FlywheelMotor{kFlywheelMotorId};
+
+    ////////////////////////////
+    // Simulation related values
+    //
+
+    // Reduction between motors and encoder, as output over input. If the flywheel
+    // spins slower than the motors, this number should be greater than one.
+    static constexpr double kFlywheelGearing = 1.0;
+
+    // @todo Figure out a real moment of inertia for the flywheel for a better simulation
+    static constexpr units::kilogram_square_meter_t kFlywheelMomentOfInertia = 0.5 * 1.5_lb * 4_in * 4_in;
+
+    frc::DCMotor m_ShooterGearbox{frc::DCMotor::KrakenX60(1)};
+    frc::LinearSystem<1, 1, 1> m_Plant{frc::LinearSystemId::FlywheelSystem(
+      m_ShooterGearbox, kFlywheelMomentOfInertia, kFlywheelGearing)};
+    frc::sim::FlywheelSim m_FlywheelSimModel{m_Plant, m_ShooterGearbox};
 };
