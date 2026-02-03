e65be5003a86dc404a3e7c3ef9041fecfed849b2
Brock Atchison <aldryd@gmail.com>
2026-02-02 20:15:46 -0600

    Add a simulated turret mechanism to TurretSubsystem

--- src/main/cpp/subsystems/TurretSubsystem.cpp
+++ src/main/cpp/subsystems/TurretSubsystem.cpp
@@ -1,4 +1,12 @@
 #include "subsystems/TurretSubsystem.h"
+
+#include "bearlog/bearlog.h"
+
+#include <frc/RobotBase.h>
+#include <frc/RobotController.h>
+#include <frc/simulation/BatterySim.h>
+#include <frc/simulation/RoboRioSim.h>
+#include <frc/util/Color8Bit.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 
 TurretSubsystem::TurretSubsystem()
@@ -7,7 +15,7 @@ TurretSubsystem::TurretSubsystem()
     ctre::phoenix6::configs::MotorOutputConfigs motorConfigs;
 
     auto& talonFXConfigurator = m_turretSpinMotor.GetConfigurator();
-    configs::CurrentLimitsConfigs limitConfigs{};
+    ctre::phoenix6::configs::CurrentLimitsConfigs limitConfigs{};
     limitConfigs.SupplyCurrentLimit = units::current::ampere_t(1);
     limitConfigs.SupplyCurrentLimitEnable = true;
     talonFXConfigurator.Apply(limitConfigs);
@@ -19,22 +27,25 @@ TurretSubsystem::TurretSubsystem()
 
     m_turretSpinMotor.SetPosition(0_tr);
 
-    m_turretSpinMotor.GetPosition().WaitForUpdate(20_ms);
-
     m_turretPID.SetIntegratorRange(-0.4, 0.4);
+
+    if (frc::RobotBase::IsSimulation()) {
+        SimulationInit();
+    }
 }
 
 void TurretSubsystem::Periodic() {
     frc::SmartDashboard::PutBoolean("Get Limit Switch", getLimitSwitch());
 
+    BearLog::Log("Turret/Setpoint", m_setpointAngle);
+    BearLog::Log("Turret/Angle", CurrentAngle());
+
     GoToAngle();
 }
 
 frc2::CommandPtr TurretSubsystem::SetGoalAngle(units::degree_t angle) {
-    m_setpointAngle = angle;
-    frc::SmartDashboard::PutNumber("Degree setpoint", m_setpointAngle.value());
-    return frc2::cmd::RunOnce([this] {
-        m_setpointAngle;
+    return frc2::cmd::RunOnce([this, angle] {
+        m_setpointAngle = angle;
     });
 }
 
@@ -47,17 +58,19 @@ bool TurretSubsystem::motorLimit() {
 }
 
 units::degree_t TurretSubsystem::CurrentAngle() {
-    units::degree_t rotation = units::degree_t(m_turretSpinMotor.GetRotorPosition().GetValueAsDouble() * 113.28);
-    if (rotation > 360_deg) {
-        rotation -= 360_deg;
-    }
-    if (rotation < 0_deg) {
-        rotation += 360_deg;
-    }
-    frc::SmartDashboard::PutNumber("CurrentAngle", rotation.value());
-    frc::SmartDashboard::PutNumber("CurrentAngleRaw", units::degree_t(m_turretSpinMotor.GetRotorPosition().GetValueAsDouble() * 360).value());
-    return rotation;
+    units::degree_t angle = GetAngleFromTurns(m_turretSpinMotor.GetPosition().GetValue());
+    return angle;
 };
+
+units::degree_t TurretSubsystem::GetAngleFromTurns(units::turn_t rotations) {
+    units::degree_t angle = units::degree_t(rotations.value() * kGearRatio);
+    return angle;
+}
+
+units::turn_t TurretSubsystem::GetTurnsFromAngle(units::degree_t angle) {
+    units::turn_t rotations = units::turn_t(angle.value() / kGearRatio);
+    return rotations;
+}
 
 bool TurretSubsystem::getLimitSwitch() {
     return !m_limitSwitch.Get();
@@ -68,11 +81,9 @@ void TurretSubsystem::GoToAngle() {
     I = frc::SmartDashboard::GetNumber("PIDTuner/I", 0) / 3;
     D = frc::SmartDashboard::GetNumber("PIDTuner/D", 0) / 3;
     m_turretPID.SetPID(P, I, D);
-    if (turretInitialized = true) {
-        double value = m_turretPID.Calculate(CurrentAngle(), m_setpointAngle);
-        frc::SmartDashboard::PutNumber("Turret PID", value);
-        m_turretSpinMotor.SetVoltage(units::volt_t(value));
-    };
+    double value = m_turretPID.Calculate(CurrentAngle(), m_setpointAngle);
+    frc::SmartDashboard::PutNumber("Turret PID", value);
+    m_turretSpinMotor.SetVoltage(units::volt_t(value));
 }
 
 void TurretSubsystem::turretInit() {
@@ -85,3 +96,35 @@ void TurretSubsystem::turretInit() {
         m_turretSpinMotor.SetVoltage(units::volt_t(0.2));
     };
 };
+
+// Runs in Simulation only!
+void TurretSubsystem::SimulationInit() {
+    const double kSimTurretLineWidth = 6;
+    m_TurretMech = m_MechRoot->Append<frc::MechanismLigament2d>("Turret", kTurretRadius.value(), 0_deg, kSimTurretLineWidth, frc::Color8Bit{frc::Color::kPurple});
+    frc::SmartDashboard::PutData("Turret Sim", &m_Mech);
+
+    auto& turret_sim = m_turretSpinMotor.GetSimState();
+    turret_sim.Orientation = ctre::phoenix6::sim::ChassisReference::CounterClockwise_Positive;
+    turret_sim.SetMotorType(ctre::phoenix6::sim::TalonFXSimState::MotorType::KrakenX60);
+}
+
+// Runs in Simulation only!
+void TurretSubsystem::SimulationPeriodic() {
+    auto& turret_sim = m_turretSpinMotor.GetSimState();
+    turret_sim.SetSupplyVoltage(frc::RobotController::GetBatteryVoltage());
+
+    auto motor_voltage = turret_sim.GetMotorVoltage();
+    m_TurretSimModel.SetInputVoltage(motor_voltage);
+
+    // Simulate the 20ms run in the simulation model
+    m_TurretSimModel.Update(20_ms);
+
+    frc::sim::RoboRioSim::SetVInVoltage(frc::sim::BatterySim::Calculate({m_TurretSimModel.GetCurrentDraw()}));
+
+    // Update the simulated state for the turret motor
+    turret_sim.SetRawRotorPosition(kGearRatio * m_TurretSimModel.GetAngle());
+    turret_sim.SetRotorVelocity(kGearRatio * m_TurretSimModel.GetVelocity());
+
+    // Update the simulated UI mechanism to the new angle based on the motor
+    m_TurretMech->SetAngle(CurrentAngle());
+}
--- src/main/include/subsystems/TurretSubsystem.h
+++ src/main/include/subsystems/TurretSubsystem.h
@@ -2,6 +2,10 @@
 
 #include <frc2/command/Commands.h>
 #include <frc2/command/SubsystemBase.h>
+#include <frc/simulation/SingleJointedArmSim.h>
+#include <frc/smartdashboard/Mechanism2d.h>
+#include <frc/smartdashboard/MechanismLigament2d.h>
+#include <frc/smartdashboard/MechanismRoot2d.h>
 
 #include <ctre/phoenix6/TalonFX.hpp>
 
@@ -15,10 +19,6 @@
 
 #include <frc2/command/button/Trigger.h>
 
-#include <frc/controller/SimpleMotorFeedforward.h>
-
-#include <subsystems/CommandSwerveDrivetrain.h>
-
 constexpr int kTurretMotorID = 60;
 
 class TurretSubsystem : public frc2::SubsystemBase {
@@ -30,6 +30,7 @@ class TurretSubsystem : public frc2::SubsystemBase {
         void goToAngle();
 
         void Periodic() override;
+        void SimulationPeriodic() override;
 
         bool motorLimit();
 
@@ -40,10 +41,14 @@ class TurretSubsystem : public frc2::SubsystemBase {
         bool turretInitialized = false;
     private:
         void GoToAngle();
+        units::degree_t GetAngleFromTurns(units::turn_t rotations);
+        units::turn_t GetTurnsFromAngle(units::degree_t angle);
 
         frc::DigitalInput m_limitSwitch{1};
 
         ctre::phoenix6::hardware::TalonFX m_turretSpinMotor;
+
+        static constexpr double kGearRatio = 113.28;
 
         static constexpr units::turns_per_second_t kMaxVelocity = 1.5_tps;
         static constexpr units::turns_per_second_squared_t kMaxAcceleration = 0.75_tr_per_s_sq;
@@ -60,8 +65,24 @@ class TurretSubsystem : public frc2::SubsystemBase {
             P, I, D, m_constraints
         };
 
-        //frc::SimpleMotorFeedforward m_turretFeedForward{kS, kV, kA};
-
         units::degree_t m_setpointAngle = 90_deg;
+
+        // Simulation specific items
+        void SimulationInit();
+        frc::Mechanism2d m_Mech{1, 1};
+        frc::MechanismRoot2d* m_MechRoot{m_Mech.GetRoot("turretRoot", 0.5, 0.5)};
+        frc::MechanismLigament2d* m_TurretMech;
+        const units::meter_t kTurretRadius = 12_in;
+        frc::DCMotor m_TurretGearbox{frc::DCMotor::KrakenX60(1)};
+        frc::sim::SingleJointedArmSim m_TurretSimModel{
+          m_TurretGearbox,
+          kGearRatio,
+          frc::sim::SingleJointedArmSim::EstimateMOI(kTurretRadius, 0.1_kg),
+          kTurretRadius,
+          -180_deg,
+          180_deg,
+          false,
+          10_deg,
+        };
 
 };
