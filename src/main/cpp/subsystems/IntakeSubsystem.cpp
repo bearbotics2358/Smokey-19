6f246b1a662b52900286012d2227abac5be71345
Bearbotics 2358 Android Stickers <bearbotics2358@gmail.com>
2026-02-10 18:13:15 -0600

    Added some functionallity for the extender in the intake subsystem. Tested with simulator, unsure whether lag in the simulator is a computer or code issue.

--- src/main/cpp/subsystems/IntakeSubsystem.cpp
+++ src/main/cpp/subsystems/IntakeSubsystem.cpp
@@ -9,9 +9,12 @@
 #include <frc/smartdashboard/SmartDashboard.h>
 
 IntakeSubsystem::IntakeSubsystem():
-    m_intakeSpinMotor(kIntakeMotorID)
+    m_intakeSpinMotor(kIntakeMotorID),
+    m_extenderMotor(kExtenderMotorID)
 {
     ctre::phoenix6::configs::MotorOutputConfigs motorConfigs;
+
+    ctre::phoenix6::configs::MotorOutputConfigs eMotorConfigs;
 
     auto& talonFXConfigurator = m_intakeSpinMotor.GetConfigurator();
     ctre::phoenix6::configs::CurrentLimitsConfigs limitConfigs{};
@@ -22,17 +25,27 @@ IntakeSubsystem::IntakeSubsystem():
     motorConfigs.WithNeutralMode(ctre::phoenix6::signals::NeutralModeValue::Brake)
         .WithInverted(false);
 
+    eMotorConfigs.WithNeutralMode(ctre::phoenix6::signals::NeutralModeValue::Brake)
+        .WithInverted(false);
+
     m_intakeSpinMotor.SetPosition(0_tr);
 
+    m_extenderMotor.SetPosition(0_tr);
+
     m_intakeSpinMotor.GetConfigurator().Apply(motorConfigs);
+    m_extenderMotor.GetConfigurator().Apply(motorConfigs);
+
+    m_extenderPID.SetIntegratorRange(-0.4, 0.4);
+
     if (frc::RobotBase::IsSimulation()) {
         SimulationInit();
     }
 }
 
 void IntakeSubsystem::Periodic() {
-    BearLog::Log("intakePeriodicRunning", true);
-    BearLog::Log("Intake/Angle", CurrentAngle());
+    BearLog::Log("IntakeExtender/Angle", CurrentAngle());
+
+    GoToAngle();
 }
 
 frc2::CommandPtr IntakeSubsystem::SpinMotor(units::volt_t volts) {
@@ -44,19 +57,49 @@ frc2::CommandPtr IntakeSubsystem::SpinMotor(units::volt_t volts) {
 }
 
 units::degree_t IntakeSubsystem::CurrentAngle() {
-    units::degree_t angle = units::degree_t(m_intakeSpinMotor.GetPosition().GetValue().value() * kGearRatio);
+    units::degree_t angle = units::degree_t(m_extenderMotor.GetPosition().GetValue().value() * kGearRatio);
     return angle;
+}
+
+frc2::CommandPtr IntakeSubsystem::SetGoalAngle(units::degree_t angle) {
+    return frc2::cmd::RunOnce([this, angle] {
+        m_setpointAngle = angle;
+    });
+}
+
+units::degree_t IntakeSubsystem::GetAngleFromTurns(units::turn_t rotations) {
+    units::degree_t angle = units::degree_t(rotations.value() * kGearRatio);
+    return angle;
+}
+
+units::turn_t IntakeSubsystem::GetTurnsFromAngle(units::degree_t angle) {
+    units::turn_t rotations = units::turn_t(angle.value() / kGearRatio);
+    return rotations;
+}
+
+void IntakeSubsystem::GoToAngle() {
+    double value = m_extenderPID.Calculate(CurrentAngle(), m_setpointAngle);
+    BearLog::Log("IntakeExtender/PIDValue", value);
+    BearLog::Log("IntakeExtender/Setpoint", m_setpointAngle);
+    m_extenderMotor.SetVoltage(units::volt_t(value));
 }
 
 // Runs in Simulation only!
 void IntakeSubsystem::SimulationInit() {
-    const double kSimIntakeLineWidth = 6;
+    const double kSimIntakeLineWidth = 16;
     m_IntakeMech = m_MechRoot->Append<frc::MechanismLigament2d>("Intake", kIntakeRadius.value(), 0_deg, kSimIntakeLineWidth, frc::Color8Bit{frc::Color::kPurple});
     frc::SmartDashboard::PutData("Intake Sim", &m_Mech);
 
     auto& intake_sim = m_intakeSpinMotor.GetSimState();
     intake_sim.Orientation = ctre::phoenix6::sim::ChassisReference::CounterClockwise_Positive;
     intake_sim.SetMotorType(ctre::phoenix6::sim::TalonFXSimState::MotorType::KrakenX60);
+
+    m_EIntakeMech = m_EMechRoot->Append<frc::MechanismLigament2d>("Turret", kEIntakeRadius.value(), 0_deg, kSimIntakeLineWidth, frc::Color8Bit{frc::Color::kPurple});
+    frc::SmartDashboard::PutData("Extender Sim", &m_EMech);
+
+    auto& extender_sim = m_extenderMotor.GetSimState();
+    extender_sim.Orientation = ctre::phoenix6::sim::ChassisReference::CounterClockwise_Positive;
+    extender_sim.SetMotorType(ctre::phoenix6::sim::TalonFXSimState::MotorType::KrakenX60);
 }
 
 // Runs in Simulation only!
@@ -64,11 +107,18 @@ void IntakeSubsystem::SimulationPeriodic() {
     auto& intake_sim = m_intakeSpinMotor.GetSimState();
     intake_sim.SetSupplyVoltage(frc::RobotController::GetBatteryVoltage());
 
+    auto& extender_sim = m_extenderMotor.GetSimState();
+    extender_sim.SetSupplyVoltage(frc::RobotController::GetBatteryVoltage());
+
     auto motor_voltage = intake_sim.GetMotorVoltage();
     m_IntakeSimModel.SetInputVoltage(motor_voltage);
 
+    auto eMotor_voltage = extender_sim.GetMotorVoltage();
+    m_EIntakeSimModel.SetInputVoltage(eMotor_voltage);
+
     // Simulate the 20ms run in the simulation model
     m_IntakeSimModel.Update(20_ms);
+    m_EIntakeSimModel.Update(20_ms);
 
     frc::sim::RoboRioSim::SetVInVoltage(frc::sim::BatterySim::Calculate({m_IntakeSimModel.GetCurrentDraw()}));
 
@@ -76,6 +126,10 @@ void IntakeSubsystem::SimulationPeriodic() {
     intake_sim.SetRawRotorPosition(kGearRatio * m_IntakeSimModel.GetAngle());
     intake_sim.SetRotorVelocity(kGearRatio * m_IntakeSimModel.GetVelocity());
 
+    extender_sim.SetRawRotorPosition(kEGearRatio * m_EIntakeSimModel.GetAngle());
+    extender_sim.SetRotorVelocity(kEGearRatio * m_EIntakeSimModel.GetVelocity());
+
     // Update the simulated UI mechanism to the new angle based on the motor
-    m_IntakeMech->SetAngle(CurrentAngle());
+    m_IntakeMech->SetAngle(0_deg);
+    m_EIntakeMech->SetAngle(CurrentAngle());
 }
--- src/main/include/subsystems/IntakeSubsystem.h
+++ src/main/include/subsystems/IntakeSubsystem.h
@@ -17,22 +17,48 @@
 #include <frc2/command/button/Trigger.h>
 
 constexpr int kIntakeMotorID = 62;
+constexpr int kExtenderMotorID = 61;
 
 class IntakeSubsystem : public frc2::SubsystemBase {
 public:
-    IntakeSubsystem();
-    frc2::CommandPtr SpinMotor(units::volt_t volts);
-    units::degree_t CurrentAngle();
+        IntakeSubsystem();
+        frc2::CommandPtr SpinMotor(units::volt_t volts);
 
-    void Periodic() override;
-    void SimulationPeriodic() override;
+        void Periodic() override;
+        void SimulationPeriodic() override;
+
+        frc2::CommandPtr SetGoalAngle(units::degree_t angle);
+        units::degree_t CurrentAngle();
 private:
+        void GoToAngle();
+
+        units::degree_t GetAngleFromTurns(units::turn_t rotations);
+        units::turn_t GetTurnsFromAngle(units::degree_t angle);
+
+
         ctre::phoenix6::hardware::TalonFX m_intakeSpinMotor;
+        ctre::phoenix6::hardware::TalonFX m_extenderMotor;
 
         static constexpr double kGearRatio = 1;
 
+        static constexpr double kEGearRatio = 360;
+
         static constexpr units::turns_per_second_t kMaxVelocity = 1.5_tps;
         static constexpr units::turns_per_second_squared_t kMaxAcceleration = 0.75_tr_per_s_sq;
+        double P = 20;
+        double I = 0.3;
+        double D = 0.0;
+
+
+        frc::TrapezoidProfile<units::turns>::Constraints m_constraints {
+            kMaxVelocity, kMaxAcceleration
+        };
+
+        frc::ProfiledPIDController<units::turns> m_extenderPID {
+            P, I, D, m_constraints
+        };
+
+        units::degree_t m_setpointAngle = 0_deg;
 
 
         void SimulationInit();
@@ -46,6 +72,22 @@ private:
           kGearRatio,
           frc::sim::SingleJointedArmSim::EstimateMOI(kIntakeRadius, 0.1_kg),
           kIntakeRadius,
+          -180_deg,
+          180_deg,
+          false,
+          10_deg,
+        };
+
+        frc::Mechanism2d m_EMech{1, 1};
+        frc::MechanismRoot2d* m_EMechRoot{m_EMech.GetRoot("extenderRoot", 360, 360)};
+        frc::MechanismLigament2d* m_EIntakeMech;
+        const units::meter_t kEIntakeRadius = 24_in;
+        frc::DCMotor m_EIntakeGearbox{frc::DCMotor::KrakenX60(1)};
+        frc::sim::SingleJointedArmSim m_EIntakeSimModel{
+          m_EIntakeGearbox,
+          kEGearRatio,
+          frc::sim::SingleJointedArmSim::EstimateMOI(kEIntakeRadius, 0.1_kg),
+          kEIntakeRadius,
           -180_deg,
           180_deg,
           false,
