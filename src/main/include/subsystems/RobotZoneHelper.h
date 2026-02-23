f96c0bccab060a425125fd3d950c1e4c11ae3224
SigmaVihaanFrFr <builderlego143@gmail.com>
2026-02-23 17:41:13 -0600

    Moved static functions to a class and made a new function to determine whether in the Robot's alliance zone

--- src/main/include/subsystems/RobotZoneHelper.h
+++ src/main/include/subsystems/RobotZoneHelper.h
@@ -1,32 +1,49 @@
+#pragma once
+
 #include <frc/geometry/Pose3d.h>
 #include <frc/geometry/Rectangle2d.h>
+#include <frc/DriverStation.h>
 
-static bool isRobotInBlueAllianceZone (frc::Pose2d botPose) {
-    frc::Rectangle2d blueAllianceZone = frc::Rectangle2d(
-        frc::Pose2d(91.05_in, 158.84_in, 
-        frc::Rotation2d()), 
-        182.11_in, 317.69_in
-    );
 
-    return blueAllianceZone.Contains(botPose.Translation());
-}
+class RobotZoneHelper {
+    public:
+        static bool isRobotInBlueAllianceZone (frc::Pose2d botPose) {
+            frc::Rectangle2d blueAllianceZone = frc::Rectangle2d(
+                frc::Pose2d(91.05_in, 158.84_in, 
+                frc::Rotation2d()), 
+                182.11_in, 317.69_in
+            );
 
-static bool isRobotInRedAllianceZone (frc::Pose2d botPose) {
-    frc::Rectangle2d redAllianceZone = frc::Rectangle2d(
-        frc::Pose2d(560.17_in, 158.84_in, 
-        frc::Rotation2d()), 
-        182.11_in, 317.69_in
-    );
+            return blueAllianceZone.Contains(botPose.Translation());
+        }
 
-    return redAllianceZone.Contains(botPose.Translation());
-}
+        static bool isRobotInRedAllianceZone (frc::Pose2d botPose) {
+            frc::Rectangle2d redAllianceZone = frc::Rectangle2d(
+                frc::Pose2d(560.17_in, 158.84_in, 
+                frc::Rotation2d()), 
+                182.11_in, 317.69_in
+            );
 
-static bool isRobotInNeutralZone (frc::Pose2d botPose) {
-    frc::Rectangle2d neutralZone = frc::Rectangle2d(
-        frc::Pose2d(325.61_in, 158.84_in, 
-        frc::Rotation2d()), 
-        287_in, 317.69_in
-    );
+            return redAllianceZone.Contains(botPose.Translation());
+        }
 
-    return neutralZone.Contains(botPose.Translation());
-}
+        static bool isRobotInNeutralZone (frc::Pose2d botPose) {
+            frc::Rectangle2d neutralZone = frc::Rectangle2d(
+                frc::Pose2d(325.61_in, 158.84_in, 
+                frc::Rotation2d()), 
+                287_in, 317.69_in
+            );
+
+            return neutralZone.Contains(botPose.Translation());
+        }
+
+        static bool isRobotInMyAllianceZone(frc::Pose2d botPose) {
+            frc::DriverStation::Alliance currentAlliance = frc::DriverStation::GetAlliance().value_or(frc::DriverStation::Alliance::kBlue);
+
+            if (currentAlliance == frc::DriverStation::Alliance::kBlue) {
+                return isRobotInBlueAllianceZone(botPose);
+            } else {
+                return isRobotInRedAllianceZone(botPose);
+            }
+        }
+};
