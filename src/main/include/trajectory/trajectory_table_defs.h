d9063dedf49ed78757e22c5580a58a76fae4750d
Bob D'Avello <bob.davello@alum.mit.edu>
2026-03-05 21:58:04 -0600

    Increase trajectory table from 4000 to 6000 RPM. Correct fixed angle shooting if the end (highest RPM) is reached. Move table size def's to a separate file.

---
+++ src/main/include/trajectory/trajectory_table_defs.h
@@ -0,0 +1,11 @@
+/* trajectory_table_defs.h - constants for the table
+
+	 created 3/5/26
+
+*/
+
+// hard coding the table to angles of 0 to 90 degrees inclusive with a step size of 1,
+// and speeds (wheel RPMs) from 1000 to 6000 inclusive with a step size of 25
+#define TRAJECTORY_TABLE_ANGLES 91
+#define TRAJECTORY_TABLE_SPEEDS 201
+
